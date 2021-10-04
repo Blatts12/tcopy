e5ad8774925209eb1aaf375ee36230da24112422
Jakub <9402720+Blatts12@users.noreply.github.com>
2021-10-04 18:01:07 +0200

    added project, created posts app

---
+++ posts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserPost(models.Model):
+    content = models.CharField(max_length=256)
+    pub_date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
