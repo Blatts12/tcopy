5001377f7a65f152759a22fd3b23991bf2017436
Jakub <9402720+Blatts12@users.noreply.github.com>
2021-10-13 18:46:12 +0200

    added cors and user serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from posts.models import UserPost
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "is_staff",
+            "is_active",
+            "date_joined",
+        ]
+
+
 class UserPostSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+
     class Meta:
         model = UserPost
         fields = "__all__"
+        read_only_fields = ["author"]
